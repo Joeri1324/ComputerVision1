5db6eb4329aa4866fae57cc8fa68a0403504d33a
joeri1324 <joeri.sleegers@student.uva.nl>
2018-03-05 15:54:58 +0100

    should be good ass 1

--- Assignment_3/harris_corner_detector.m
+++ Assignment_3/harris_corner_detector.m
@@ -1,7 +1,6 @@
+function [H, r, c] = harris_corner_detector(I, treshold)
 
-J = im2double(imread('person_toy/00000001.jpg'));
-I = im2double(imread('pingpong/0000.jpeg'));
-
+grayI = rgb2gray(I)
 % Gaussian derivative filters to x and to y
 G = fspecial('gauss', [3, 3]);
 Gx = fspecial('gauss', [1, 3]);
@@ -10,13 +9,35 @@ Gdx = gradient(Gx);
 Gdy = gradient(Gy);
 
 % Images smoothed with gaussian derivatives
-Ix = imfilter(I, Gdx);
-Iy = imfilter(I, Gdy);
+Ix = imfilter(grayI, Gdx);
+Iy = imfilter(grayI, Gdy);
 
-% Caclculate Q(x,y) of auto-correlation
+% Calculate Q(x,y) of auto-correlation
 A = imfilter(Ix.^2, Gx);
 C = imfilter(Iy.^2, Gy);
 B = imfilter(Ix.*Iy, G);
 
 % Cornerness of each pixel
 H = (A.*C - B.^2) - 0.04*((A+C).^2);
+
+% Check corners
+[r, c] = find(H > treshold);
+
+% set corners to green
+for i = 1:size(r)
+    I(r(i), c(i), :) = [0; 1; 0];
+end
+
+subplot(2, 2, 1)
+imshow(Ix)
+title('gradient x direction')
+
+subplot(2, 2, 2)
+imshow(Iy)
+title('gradient y direction')
+
+subplot(2, 2, 3)
+imshow(I)
+title('corners detected by harris')
+
+end
---
+++ Assignment_3/main.m
@@ -0,0 +1,21 @@
+%%
+J = im2double(imread('person_toy/00000001.jpg'));
+I = im2double(imread('pingpong/0000.jpeg'));
+
+figure
+harris_corner_detector(I, 0.005);
+
+figure
+harris_corner_detector(J, 0.005);
+
+%% Rotated image
+
+rotatedJ = imrotate(J, randi([0, 360]));
+harris_corner_detector(rotatedJ, 0.005);
+
+%% Optical flow
+
+I = im2double(imread('sphere1.ppm'));
+J = im2double(imread('sphere2.ppm'));
+
+lucas_kanade(I, J)
