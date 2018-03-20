311f9dc5b146e6df6438704f2f05130ac8e6e445
joeri1324 <joeri.sleegers@student.uva.nl>
2018-03-20 14:27:37 +0100

    update

--- finalProject/part2/finetune_cnn.m
+++ finalProject/part2/finetune_cnn.m
@@ -20,7 +20,7 @@ opts.train = struct() ;
 opts = vl_argparse(opts, varargin) ;
 if ~isfield(opts.train, 'gpus'), opts.train.gpus = []; end;
 
-opts.train.gpus = [1];
+opts.train.gpus = [];
 
 
 
@@ -84,14 +84,38 @@ splits = {'train', 'test'};
 
 %% TODO: Implement your loop here, to create the data structure described in the assignment
 
+airplanesTrain = readInData('./ImageData/airplanes_train/');
+carsTrain = readInData('./ImageData/cars_train/');
+facesTrain = readInData('./ImageData/faces_train/');
+motorbikesTrain = readInData('./ImageData/motorbikes_train/');
 
-%%
-% subtract mean
+airplanesTest = readInData('./ImageData/airplanes_test/');
+carsTest = readInData('./ImageData/cars_test/');
+facesTest = readInData('./ImageData/faces_test/');
+motorbikesTest = readInData('./ImageData/motorbikes_test/');
+
+data = cat(4, airplanesTrain, carsTrain, facesTrain, motorbikesTrain, airplanesTest, carsTest, facesTest, motorbikesTest);
+training_size = size(airplanesTrain, 4) + size(carsTrain, 4) + size(facesTrain, 4) + size(motorbikesTrain, 4);
+test_size = size(airplanesTest, 4) + size(carsTest, 4) + size(facesTest, 4) + size(motorbikesTest, 4);
+
+sets = [ones([training_size, 1]); ones([test_size, 1]) + 1];
+
+labels = [
+    ones([size(airplanesTrain, 4), 1]);
+    ones([size(carsTrain, 4), 1]) + 1;
+    ones([size(facesTrain, 4), 1]) + 2;
+    ones([size(motorbikesTrain, 4), 1]) + 3;
+    ones([size(airplanesTest, 4), 1]);
+    ones([size(carsTest, 4), 1]) + 1;
+    ones([size(facesTest, 4), 1]) + 2;
+    ones([size(motorbikesTest, 4), 1]) + 3;
+];
+
 dataMean = mean(data(:, :, :, sets == 1), 4);
 data = bsxfun(@minus, data, dataMean);
 
 imdb.images.data = data ;
-imdb.images.labels = single(labels) ;
+imdb.images.labels = single(labels') ;
 imdb.images.set = sets;
 imdb.meta.sets = {'train', 'val'} ;
 imdb.meta.classes = classes;
@@ -101,4 +125,25 @@ imdb.images.data = imdb.images.data(:,:,:, perm);
 imdb.images.labels = imdb.images.labels(perm);
 imdb.images.set = imdb.images.set(perm);
 
+end
+
+function [images] = readInData(folderName)
+
+    imagefiles = dir(strcat(folderName, '*.jpg')); 
+    
+    nfiles = length(imagefiles);    % Number of files found
+    
+    image = imread(strcat(folderName, imagefiles(1).name));
+    images = zeros(32, 32, 3, nfiles);
+
+    for ii=1:nfiles
+       currentfilename = strcat(folderName, imagefiles(ii).name);
+       image = imread(currentfilename);  
+       currentimage = imresize(im2double(image), [32 32]);
+       if size(currentimage, 3) == 1
+           currentimage = cat(3, currentimage, currentimage, currentimage);
+       end
+  
+       images(:, :, :, ii) = currentimage;  
+    end
 end
